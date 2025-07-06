01564a32d0902e4b0fead4935daeadd441279349
Devendra <shresthadevendra2056@gmail.com>
2025-07-06 11:00:01 +0545

    Daily commit on 2025-07-06 11:00:01

--- class/day1.py
+++ class/day1.py
@@ -31,71 +31,106 @@ match day:
 # print(b.strftime('%A'),a.strftime('%H:%M:%S'))
 
 
-class A:
-    def __init__(self, fname, lname):
-        self.firstname = fname
-        self.lastname = lname
+# class A:
+#     def __init__(self, fname, lname):
+#         self.firstname = fname
+#         self.lastname = lname
 
-    def printname(self):
-        print(self.firstname, self.lastname)
+#     def printname(self):
+#         print(self.firstname, self.lastname)
 
-class B(A):
-    def __init__(self, fname, lname, address):
-        A.__init__(self, fname, lname)  
-        self.address = address
+# class B(A):
+#     def __init__(self, fname, lname, address):
+#         A.__init__(self, fname, lname)  
+#         self.address = address
 
-    def printaddress(self):
-        print(self.address)
+#     def printaddress(self):
+#         print(self.address)
 
-class C(B):
-    def __init__(self, fname, lname, address):
-        B.__init__(self, fname, lname, address)  
-
-
-x = B("Prami", "Olsen", "Kathmandu")
-
-x.printname()        
-x.printaddress()  
+# class C(B):
+#     def __init__(self, fname, lname, address):
+#         B.__init__(self, fname, lname, address)  
 
 
+# x = B("Prami", "Olsen", "Kathmandu")
 
-class A:
-    def __init__(self, fname, lname):
-        self.firstname = fname
-        self.lastname = lname
-
-    def printname(self):
-        print(self.firstname, self.lastname)
-
-class B(A):
-    def __init__(self, fname, lname, address):
-        super().__init__(fname, lname)  
-        self.address = address
-
-    def printaddress(self):
-        print(self.address)
-
-class C(B):
-    def __init__(self, fname, lname, address):
-        super().__init__( fname, lname, address)  
+# x.printname()        
+# x.printaddress()  
 
 
-x = B("Prami", "Olsen", "Kathmandu")
 
-x.printname()        
-x.printaddress()
+# class A:
+#     def __init__(self, fname, lname):
+#         self.firstname = fname
+#         self.lastname = lname
+
+#     def printname(self):
+#         print(self.firstname, self.lastname)
+
+# class B(A):
+#     def __init__(self, fname, lname, address):
+#         super().__init__(fname, lname)  
+#         self.address = address
+
+#     def printaddress(self):
+#         print(self.address)
+
+# class C(B):
+#     def __init__(self, fname, lname, address):
+#         super().__init__( fname, lname, address)  
 
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name      # attribute
-        self.age = age        # attribute
+# x = B("Prami", "Olsen", "Kathmandu")
 
-    def greet(self):
-        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+# x.printname()        
+# x.printaddress()
 
-# 🔸 Create an object of the class
-person1 = Person("Devendra", 25)
 
-# 🔸 Access method using object
-person1.greet()
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name      # attribute
+#         self.age = age        # attribute
+
+#     def greet(self):
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# # 🔸 Create an object of the class
+# person1 = Person("Devendra", 25)
+
+# # 🔸 Access method using object
+# person1.greet()
+
+
+
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# user_input = int(input("Enter a number to check if it's prime: "))
+# if is_prime(user_input):
+#     print(f"{user_input} is a prime number.")
+# else:
+#     print(f"{user_input} is not a prime number.") 
+
+
+def is_prime(i,j):
+    if i <= 1:
+        return False
+    for n in range(2, i):
+        if i % n == 0:
+            return False
+    return True
+
+for i in range(2, 101):
+    if is_prime(i, 0):
+        print(f"{i} is a prime number.")
+
+user_input = int(input("Enter a number to check if it's prime: "))
+if is_prime(user_input, 0):
+    print(f"{user_input} is a prime number.")
+else:     
+    print(f"{user_input} is not a prime number.")         
